9bc4aab4871640cb2a5e9695aa3d428c3194e8fa
tjennat <theo.jennat98@gmail.com>
2024-04-08 14:05:18 +0200

    fixing PEP8

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -3,10 +3,12 @@
 from flask import Flask
 app = Flask(__name__)
 
+
 # Route definition with strict_slashes=False
 @app.route('/', strict_slashes=False)
 def hello():
     return "Hello HBNB!"
+
 
 if __name__ == '__main__':
     # Run the Flask application on 0.0.0.0, port 5000
