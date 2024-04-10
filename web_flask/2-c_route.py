8b5c6355db975060229c69df0f8f380960faeef5
tjennat <theo.jennat98@gmail.com>
2024-04-10 11:05:32 +0200

    routing finally my flask app correctly

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,35 +1,26 @@
-from flask import Flask, escape, abort
+#!/usr/bin/python3
+"""routing my flask app"""
+
+from flask import Flask
+
 
 app = Flask(__name__)
 
 
-# Route definition with strict_slashes=False
 @app.route('/', strict_slashes=False)
-def hello():
-    return "Hello HBNB!"
+def hello_hbnb():
+    return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
-def hbnb():
-    return "HBNB"
+def display_hbnb():
+    return 'HBNB'
 
 
-@app.route('/c/', strict_slashes=False)
 @app.route('/c/<text>', strict_slashes=False)
-def c(text='is_fun'):
-    return "C {}".format(escape(text).replace('_', ' '))
-
-
-# Custom error handler for 404 Not Found
-@app.errorhandler(404)
-def page_not_found(e):
-    return "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 3.2 Final//EN\">\n" \
-           "<title>404 Not Found</title>\n" \
-           "<h1>Not Found</h1>\n" \
-           "<p>The requested URL was not found on the server. " \
-           "If you entered the URL manually please check your spelling and try again.</p>", 404
+def display_c(text):
+    return 'C {}'.format(text.replace("_", " "))
 
 
 if __name__ == '__main__':
-    # Run the Flask application on 0.0.0.0, port 5000
     app.run(host='0.0.0.0', port=5000)
