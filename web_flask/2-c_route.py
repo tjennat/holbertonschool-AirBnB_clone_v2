6c1b3946b24a0bef78a48443af55f7e57e96c0b1
tjennat <theo.jennat98@gmail.com>
2024-04-10 10:38:19 +0200

    script that starts a web app using routes

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+from flask import Flask, escape
+
+app = Flask(__name__)
+
+# Route definition with strict_slashes=False
+@app.route('/', strict_slashes=False)
+def hello():
+    return "Hello HBNB!"
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    return "C {}".format(escape(text).replace('_', ' '))
+
+if __name__ == '__main__':
+    # Run the Flask application on 0.0.0.0, port 5000
+    app.run(host='0.0.0.0', port=5000)
