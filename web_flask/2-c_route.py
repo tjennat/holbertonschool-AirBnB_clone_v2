3f64f6a91c9a5b27dc78ec58fe043cf57c914f88
tjennat <theo.jennat98@gmail.com>
2024-04-10 10:58:02 +0200

    adding error handler

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,6 +1,4 @@
-#!/usr/bin/python3
-"""routing my flask app"""
-from flask import Flask, escape
+from flask import Flask, escape, abort
 
 app = Flask(__name__)
 
@@ -16,9 +14,20 @@ def hbnb():
     return "HBNB"
 
 
+@app.route('/c/', strict_slashes=False)
 @app.route('/c/<text>', strict_slashes=False)
-def c(text):
+def c(text='is_fun'):
     return "C {}".format(escape(text).replace('_', ' '))
+
+
+# Custom error handler for 404 Not Found
+@app.errorhandler(404)
+def page_not_found(e):
+    return "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 3.2 Final//EN\">\n" \
+           "<title>404 Not Found</title>\n" \
+           "<h1>Not Found</h1>\n" \
+           "<p>The requested URL was not found on the server. " \
+           "If you entered the URL manually please check your spelling and try again.</p>", 404
 
 
 if __name__ == '__main__':
